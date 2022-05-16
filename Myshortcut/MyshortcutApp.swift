e9f04c904fcaef0332cbb2f26d4e0699ce5b627b
NothingButBugs <strong_ji@outlook.com>
2022-05-16 13:36:14 -0400

    first commit

---
+++ Myshortcut/MyshortcutApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyshortcutApp.swift
+//  Myshortcut
+//
+//  Created by Sizhuang on 5/16/22.
+//
+
+import SwiftUI
+
+@main
+struct MyshortcutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
